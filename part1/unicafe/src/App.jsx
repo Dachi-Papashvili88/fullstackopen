5bcfa078f8a0fe312b2581683bc3662670b3e65f
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2024-05-12 20:16:51 +0000

    create buttons and corresponding statistics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <h4>good {good}</h4>
+      <h4>neutral {neutral}</h4>
+      <h4>bad {bad}</h4>
+    </div>
+  );
 };
 
 export default App;
