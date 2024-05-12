bb8cad8d4abc2deb8fb7678b0f2810c7398fc61e
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2024-05-12 21:28:46 +0000

    add all, avarage and positive feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,6 +15,12 @@ const App = () => {
       <h4>good {good}</h4>
       <h4>neutral {neutral}</h4>
       <h4>bad {bad}</h4>
+      <h4>all {good + neutral + bad}</h4>
+      <h4>
+        average{" "}
+        {(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad) || 0}
+      </h4>
+      <h4>positive {(good / (good + neutral + bad) || 0) * 100}%</h4>
     </div>
   );
 };
